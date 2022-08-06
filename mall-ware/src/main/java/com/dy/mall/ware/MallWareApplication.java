33f086eadf4e91eed34a4b0cf65fb2bb3dadba9f
bsgjenkins <bsgjenkins@supremind.com>
2022-08-06 10:42:05 +0800

    feat:项目结构初始化

---
+++ mall-ware/src/main/java/com/dy/mall/ware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package com.dy.mall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
